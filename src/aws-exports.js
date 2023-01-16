288bbd1b4df0f61258092fec24cd1259fd299e82
LaraRobertson <lara@lararobertson.com>
2023-01-16 14:11:53 -0500

    added storage for images

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://ego2gtyk4fdqni5xhpyk6nnxha.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-rgryroqgsbd5pkgegqhwh2vf7u",
+    "aws_user_files_s3_bucket": "escapeoutbucket213334-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
